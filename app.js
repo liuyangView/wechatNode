6afb3eb1d5571a94533eee27ffe26f61d751c5a9
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2017-10-18 12:05:26 +0800

    t

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+
 var wechatApp = new wechat(config); //实例wechat 模块
 
 //用于处理所有进入 3000 端口 get 的连接请求
