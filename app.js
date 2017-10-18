49a647c9a79632c0cd3b122938b3816e4574d2c6
yaoyaodu <yaoyaodu@YYBTs-mac.local>
2017-10-18 12:07:36 +0800

    t

--- app.js
+++ app.js
@@ -6,19 +6,13 @@ const express = require('express'), //express 框架
 var app = express();//实例express框架
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
 // create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 // parse various different custom JSON types as JSON
-app.use(bodyParser.json({ type: 'application/*+json' }))
-
-// parse some custom thing into a Buffer
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
-
-// parse an HTML body into a string
-app.use(bodyParser.text({ type: 'text/html' }))
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
 
 var wechatApp = new wechat(config); //实例wechat 模块
